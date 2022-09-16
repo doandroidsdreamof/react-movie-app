bcfbc8a39116d8feaa39a9cd44e1cb36e6fd5f8f
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-09-16 05:47:58 +0300

    carousel added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Card from './components/Card.jsx';
 import SideNav from './components/SideNav.jsx';
-import Carousel from './components/Carousel.jsx';
 import Layout from './components/Layout.jsx';
 
  function App() {
   return (
-      <div className='flex flex-wrap h-screen border-2 w-fit border-red-400'>
-       <Layout />
+    <>
+
+      <div className=' min-h-screen relative  bg-bg-color flex justify-between'>
+      <SideNav />
+
+      <Layout />
+
       </div>
-  )
+      </>
+  )    
 }
 
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,19 +2,16 @@ import React from "react";
 import Card from './Card.jsx';
 import Search from './Search.jsx';
 import SideNav from './SideNav.jsx';
-import Carousel from './Carousel.jsx';
 import ArtBoard from './ArtBoard.jsx';
 
 
 const Layout = () => {
   return (
     <>
-    <div className=" h-screen bg-bg-color w-screen  flex reative">
-      <SideNav />
-      <ArtBoard />
-  
 
-  </div>
+      <ArtBoard />
+
+
   </>
   );
 };
