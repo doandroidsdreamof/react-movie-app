87dc72b89cce17dc325039422c80cdfa492a1dca
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-09-25 04:34:55 +0300

    sidenav-functionality

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -11,9 +11,8 @@ import { useState, useEffect } from "react";
 
 const SideNav = () => {
   const [menu, setMenu] = useState(false);
-
   useEffect(() => {
-
+    
 
   }, [menu]);
 
@@ -39,10 +38,10 @@ const SideNav = () => {
         <li className=" hover:bg-gray-800 cursor-pointer sm:justify-start px-4  h-12  flex items-center justify-center active text-center ">
           <label className=" swap swap-rotate">
             <input onClick={(e) => handleClick(e)} type="checkbox" />
-            <svg className="swap-off fill-current   " xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 512 512">
+            <svg className="swap-on fill-current   md:-translate-x-1" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 512 512">
               <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
             </svg>
-            <svg className="swap-on fill-current  -translate-x-1" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 512 512">
+            <svg className="swap-off fill-current  " xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 512 512">
               <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
             </svg>
             <span className="all-hide ml-10 mt-1  pr-11 hidden sm:block  text-gray-400 font-semibold tracking-wide hover:text-white transition-colors">Close Menu</span>
@@ -74,7 +73,7 @@ const SideNav = () => {
         </li>
         <li className="hover:bg-gray-800 cursor-pointer sm:justify-start px-4  h-12  flex items-center justify-center">
           <DarkMode />
-          <span className="all-hide ml-3 hidden sm:block  text-gray-400 font-semibold tracking-wide hover:text-white transition-colors">Dark Mode</span>
+          <span className="all-hide ml-2 mt-1 hidden sm:block  text-gray-400 font-semibold tracking-wide hover:text-white transition-colors">Dark Mode</span>
         </li>
       </ul>
     </div>
