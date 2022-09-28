aebe739bfa4b1089be020eae7748d9a5a2e04837
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-09-28 05:43:03 +0300

    sign-up form page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import SideNav from './components/SideNav.jsx';
 import Row from './components/Row.jsx';
 import Home from './pages/Home.jsx'
 import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
 import FormLayout from './components/FormLayout.jsx'
+
+
+SignUp
  function App() {
   return (
     <>
     <FormLayout>
-    <SignIn />
+    <SignUp />
     </FormLayout>
 
 
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -8,20 +8,20 @@ const SignIn = () => {
         <div className="rounded-xl  bg-opacity-75 bg-input-card backdrop-blur-2xl  shadow-xl">
           <div className="lg:grid lg:grid-cols-2">
             <div className="rounded-lg lg:block" hidden>
-              <img src={signImage} className="rounded-l-xl object-cover   h-onehundred"  height="" width="" alt="sign-image" />
+              <img src={signImage} className="rounded-l-xl object-cover   h-onehundred" height="" width="" alt="sign-image" />
             </div>
             <div className="p-6 sm:p-16">
               <h2 className="mb-8 text-2xl text-cyan-900 font-bold">Sign in to your account</h2>
               <form action="" className="space-y-8">
                 <div className="space-y-2">
-                  <label for="email" className="text-gray-700">
-                    Email
+                  <label for="email" className="text-gray-700">             
                   </label>
                   <input
                     type="email"
                     name="email"
                     id="email"
-                    className="block w-full px-4 py-3 rounded-md border border-gray-300 text-gray-600 transition duration-300
+                    placeholder="email"
+                    className="block w-full px-4 py-3 rounded-md border bg-gray-200 border-gray-300 text-gray-600 transition duration-300
         focus:ring-2 focus:ring-sky-300 focus:outline-none
         invalid:ring-2 invalid:ring-red-400"
                   />
@@ -29,7 +29,7 @@ const SignIn = () => {
                 <div>
                   <div className="flex items-center justify-between">
                     <label for="pwd" className="text-gray-700">
-                      Password
+                      
                     </label>
                     <button className="p-2 -mr-2" type="reset">
                       <span className="text-sm text-sky-500">Forgot your password ?</span>
@@ -39,12 +39,13 @@ const SignIn = () => {
                     type="password"
                     name="pwd"
                     id="pwd"
-                    className="block w-full px-4 py-3 rounded-md border border-gray-300 text-gray-600 transition duration-300
+                    placeholder="password"
+                    className="block w-full px-4  bg-gray-200 py-3 rounded-md border border-gray-300 text-gray-600 transition duration-300
                                             focus:ring-2 focus:ring-sky-300 focus:outline-none
                                             invalid:ring-2 invalid:ring-red-400"
                   />
                   <div className=" flex w-full mt-4 flex-row h-11 text-center justify-center">
-                    <button type="button" className="text-white 1  justify-center  text-xs w-full bg-[#343a42] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg px-5 py-1 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2">
+                    <button type="button" className="text-white 1  justify-center  text-xs w-full bg-[#3f4d5f] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-md  py-1 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2">
                       <svg className="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
                         <path
                           fill="currentColor"
@@ -53,7 +54,7 @@ const SignIn = () => {
                       </svg>
                       Sign in with Github
                     </button>
-                    <button type="button" className="text-white justify-center  text-xs w-full bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg  px-5 py-1 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
+                    <button type="button" className="text-white justify-center  text-xs w-full bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-md  py-1 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
                       <svg className="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
                         <path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
                       </svg>
