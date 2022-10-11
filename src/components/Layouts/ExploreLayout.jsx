5e6e7678c2dbc1cad77596f9d2cf75cab73c0e6c
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-10-11 19:43:42 +0300

    explore movies cards

--- src/components/Layouts/ExploreLayout.jsx
+++ src/components/Layouts/ExploreLayout.jsx
@@ -2,71 +2,33 @@ import React from "react";
 
 const ExploreLayout = (props) => {
   return (
-    <section className="flex   h-screen justify-around  overflow  bg-bg-color">
+    <section className="flex  h-screen justify-around  overflow  bg-bg-color">
       <div className="flex flex-1 flex-row justify-between overflow-x-hidden   overflow-y-auto ">
         <div className="Side-nav  flex  order-1">{props.nav}</div>
         <div className=" w-full  order-2 flex flex-col   ">
           <h1 className="bg-gradient-to-r mx-auto mt-3 from-star via-purple-400 to-purple-600 bg-clip-text text-4xl font-extrabold text-transparent sm:text-5xl">Explore Movies</h1>
           <div className=" pt-12  gap-4   w-full justify-items-center  lg:justify-center items-start flex flex-col lg:flex-row ">
             {props.searchBar}</div>
-          <div className=" lg:h-full h-1/2  px-7 md:px-0 mt-5 w-full md:w-fit  lg:mx-0 lg:ml-auto lg:-translate-x-4  mx-auto ">
-       <div className="flex flex-col gap-y-5 md:flex-row lg:flex-col md:gap-x-3 md:justify-center ">
+          <div className=" lg:h-1/2 h-1/4   px-7 md:px-0 mt-5 w-full md:w-fit   lg:ml-auto lg:-translate-x-4  mx-auto ">
+       <div className="flex flex-col gap-y-5 md:flex-row  md:gap-x-3 md:justify-center ">
        {props.sortInput}
-            {props.filterInput}
+        {props.filterInput}
        </div>
-          </div>
+   </div>
+   <div className={`card-place mt-4  h-full full  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  justify-evenly gap-2  lg:gap-x-6 lg:gap-y-10 lg:px-20    px-6 md:px-10 `}>
+          {props.movies}
+
+  
+    </div>
         </div>
+
+          
       </div>
+
     </section>
   );
 };
 
 export default ExploreLayout;
 
-/*
-<section class="bg-gray-900 text-white">
-  <div
-    class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
-  >
-    <div class="mx-auto max-w-3xl text-center">
-      <h1
-        class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
-      >
-        Understand User Flow.
 
-        <span class="sm:block"> Increase Conversion. </span>
-      </h1>
-
-      <p class="mx-auto mt-4 max-w-xl sm:text-xl sm:leading-relaxed">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
-        tenetur fuga ducimus numquam ea!
-      </p>
-
-      <div class="mt-8 flex flex-wrap justify-center gap-4">
-        <a
-          class="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-          href="/get-started"
-        >
-          Get Started
-        </a>
-
-        <a
-          class="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
-          href="/about"
-        >
-          Learn More
-        </a>
-      </div>
-    </div>
-  </div>
-</section>
-
-
-
-
-
-
-
-
-
-*/
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -7,29 +7,85 @@ import SearchBar from "../components/AsideBar/SearchBar.jsx";
 import {useEffect,useState} from 'react'
 import FilterInput from '../components/Explore/FilterInput.jsx'
 import SortInput from '../components/Explore/SortInput.jsx'
-
-
+import InfiniteScroll from 'react-infinite-scroll-component';
+import data from '../../Data'
+import Movie from '../components/Movie.jsx'
 
 
  const Explore = () => {
  const [searchLogic,setSearchLogic] = useState(false)
-useEffect(() =>{
-  setSearchLogic(!searchLogic)
+ const [initialMovies,setInitialMovies] = useState([])
 
+useEffect(() =>{
+setSearchLogic(!searchLogic)
+fetch(data.requestTopRated)
+.then((res) => res.json())
+.then((get) => setInitialMovies(get.results))
+.catch(err => {console.error(err)})
 
 },[])
 
 
+console.log('here', initialMovies)
+
   return (
+    <>
   <ExploreLayout 
    nav={<SideNav />}
    searchBar={<SearchBar explorePage={searchLogic} />}
    filterInput={<FilterInput />}
    sortInput={<SortInput />}
+   movies={initialMovies.map((el, id) => (
+    <Movie explore={el} />
+          ))}
 
   
   />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </>
   )
 }
 
 export default Explore
+
+/*
+<InfiniteScroll
+  dataLength={items.length} //This is important field to render the next data
+  next={fetchData}
+  hasMore={true}
+  loader={<h4>Loading...</h4>}
+  endMessage={
+    <p style={{ textAlign: 'center' }}>
+      <b>Yay! You have seen it all</b>
+    </p>
+  }
+  // below props only if you need pull down functionality
+  refreshFunction={this.refresh}
+  pullDownToRefresh
+  pullDownToRefreshThreshold={50}
+  pullDownToRefreshContent={
+    <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
+  }
+  releaseToRefreshContent={
+    <h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>
+  }
+>
+  {items}
+</InfiniteScroll>
+
+*/
