436ba6c297670824a8e016483d1f918873090a7d
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-10-13 15:10:46 +0300

    infinite scroll loading bar

--- src/components/Explore/MovieExplore.jsx
+++ src/components/Explore/MovieExplore.jsx
@@ -3,40 +3,43 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import { useEffect, useState } from "react";
 import { Link  } from "react-router-dom";
 import "react-lazy-load-image-component/src/effects/blur.css";
-
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 
 
 const MovieExplore = ({ explore,expolorePage }) => {
   const [loaded,setLoaded]= useState(false)
 
+
   useEffect(() =>{
-    setLoaded(false)
+
   },[])
- 
+
+  console.log(explore)
+  const addElipsisTitle = explore?.title.length > 20 ? explore?.title.substring(0, 100) + "..." : explore?.title;
+
  
   return (
 
-<>
-<div className="relative my-5  bg-white  dark:bg-[#18191c] shadow-xl hover:shadow duration-200 rounded-xl group">
-  <div className="relative w-full rounded-xl">
-  <Link  to={`/movie/${ explore.id}`} ><LazyLoadImage afterLoad={() => {setLoaded(true)}} effect="blur" className="object-cover h-60 hover:opacity-95 rounded-md transform  " src={`https://image.tmdb.org/t/p/w342/${ explore?.poster_path}`} alt={ explore?.title} /></Link>
 
-
-    <div className="absolute top-3 left-4 flex items-center space-x-2 cursor-pointer">
- 
-    </div>
-  </div>
-  <div className="absolute bottom-0 left-0 right-0 rounded-b-xl bg-white bg-opacity-50 backdrop-filter backdrop-blur-sm p-4 group-hover:py-6 duration-300">
+<div className="relative  justify-center  h-fit flex w-full scale-75 md:scale-90 bg-card-second shadow-xl hover:shadow duration-200 rounded-xl group transition  md:hover:scale-95 ease-in ">
+{!loaded ? <Box   sx={{position: 'absolute', display: 'flex', zIndex:50, top:'50%' }}>
+      <CircularProgress />
+    </Box> : <></>}
+    
+  <div className="relative rounded-xl bg-card-second w-full h-96">
+  <Link  to={`/movie/${ explore.id}`} ><LazyLoadImage afterLoad={() => {setLoaded(true)}} effect="blur" className="object-cover h-96 w-screen hover:opacity-95 rounded-md transform  " src={`https://image.tmdb.org/t/p/w342/${ explore?.poster_path}`} alt={ explore?.title} /></Link>
+  <div className="absolute  bottom-0 left-0 right-0 rounded-b-md bg-white bg-opacity-50 backdrop-filter backdrop-blur-sm p-4 group-hover:py-6 duration-300">
     <h5 className="text-primary font-medium text-sm">
-      Segoe cute
+      {explore.title}
     </h5>
-    <small className="text-xs font-light text-primary">
-      Do you want to participate?
-    </small>
   </div>
+
+  </div>
+
 </div>
-</>
+
 
 
   );
--- src/components/Layouts/ExploreLayout.jsx
+++ src/components/Layouts/ExploreLayout.jsx
@@ -4,6 +4,8 @@ import data from '../../../Data'
 import MovieExplore from '../Explore/MovieExplore.jsx'
 import SearchBar from '../AsideBar/SearchBar.jsx'
 
+
+
 function ExploreLayout(props) {
     const [initialMovies, setInitialMovies] = useState([])
     const [page, setPage] = useState(1)
@@ -21,7 +23,6 @@ function ExploreLayout(props) {
         setPage(page < 1000 ? page + 1 : page)
     }
 
-    console.log('here =>', page)
 
     return (
         <section className=" flex overflow flex-row  bg-bg-color ">
@@ -30,16 +31,16 @@ function ExploreLayout(props) {
             <div className="flex flex-col w-full overflow-hidden ">
  
  
-                <div className="w-full    max-h-screen border   bg-bg-color ">
+                <div className="w-full    max-h-screen    bg-bg-color ">
       
           
-                    <div id="scrollableDiv" className='h-onehundred  overflow-y-scroll flex flex-col gap-y-10'>
-                    <h1 className="bg-gradient-to-r mx-auto mt-3 from-star via-purple-400 to-purple-600 bg-clip-text text-4xl font-extrabold text-transparent sm:text-5xl">
+                    <div id="scrollableDiv" className='h-onehundred  overflow-y-scroll flex flex-col gap-y-7'>
+                    <h1 className="bg-gradient-to-r mx-auto mt-3 from-star via-purple-400 to-purple-600 bg-clip-text text-4xl font-extrabold text-transparent sm:text-5xl ">
                             Explore Movies
                         </h1>
                 <SearchBar  expolorePage={props.logic}/>
                    
-                            <div className="flex flex-col gap-y-5 md:flex-row justify-center md:gap-x-3 md:justify-center ">
+                            <div className="flex flex-col gap-y-5 px-6 md:px-0 md:flex-row justify-center md:gap-x-3 md:justify-center ">
                                 {props.sortInput}
                                 {props.filterInput}
                             </div>
@@ -47,11 +48,11 @@ function ExploreLayout(props) {
                             dataLength={initialMovies.length}
                             next={infiniteIncrease}
                             hasMore={true}
-                            loader={<h4>Loading...</h4>}
+                            loader={<h4></h4>}
                             scrollableTarget="scrollableDiv"
                         >
                             <div
-                                className={`card-place  justify-center overflow-hidden mt-12 order-3 h-onehundred grid-flow-row    w-full   grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-2  lg:gap-x-6 lg:gap-y-10 lg:px-20    px-6 md:px-10 `}
+                                className={`card-place   items-center   overflow-hidden mt-12 order-3 h-onehundred   w-full   grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-2   lg:px-32 lg:gap-0   px-6 md:px-10 `}
                             >
                                 {initialMovies.map((el, id) => (
                                     <MovieExplore key={id} explore={el} />
