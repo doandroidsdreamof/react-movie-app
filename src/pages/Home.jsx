24eea0f5253425773a8a02189315e605002ea366
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-10-16 14:41:08 +0300

    navigation bar props name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ import data from "../../Data.js";
 const Home = () => {
   return (
     <PageLayout
-    SideNav={<SideNav />}
+    sideNav={<SideNav />}
       aside={<Aside  HomePage={"HomePage"} trendData={data.requestDayTrending} />}
       slider={<TopSlider sliderId="0" endpoint={data.requestWeekTrending} />}
       row={
