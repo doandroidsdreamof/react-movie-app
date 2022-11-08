3ab7c55c237609665b84508a12c70d1b0063cc07
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2022-11-08 03:02:50 +0300

    forms button alignment

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,75 +1,14 @@
 import React from 'react'
 import signImage from '../assets/image/sign-image2.jpg'
-import { Link,useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useFormik } from 'formik'
-import { useEffect, useState,useContext } from 'react'
-import * as Yup from 'yup';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { auth} from '../firebase'
-import {AuthContext} from "../context/AuthContext"
-
-const validate = (values) => {
-    const errors = {}
-
-    if (!values.email) {
-        errors.email = 'Required'
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address'
-    }
-    return errors
-}
-
-
-
+import { useEffect, useState, useContext } from 'react'
+import * as Yup from 'yup'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../firebase'
+import { AuthContext } from '../context/AuthContext'
 
 const SignIn = () => {
-    const [password, setPassword] = useState('')
-    const [email, setEmail] = useState('')
-    const [error, setError] = useState(false);
-    const navitage = useNavigate()
-    const auth = getAuth();
-    const {dispatch} = useContext(AuthContext)
-
-    const errors = {}
-
-    useEffect(() =>{
-
-    },[])
-
-
-
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-        },
-
-        validate,
-
-        onSubmit: (values) => {
-          JSON.stringify(values, null, 2)
-          setPassword(values.password)
-          setEmail(values.email)
-          handleLogin(values)
-
-        },
-    })
-
-
-     function handleLogin (values) {
-        signInWithEmailAndPassword(auth, values.email, values.password)
-          .then((userCredential) => {
-            // Signed in
-            const user =  userCredential.user;
-            console.log(user)
-            dispatch({type:"LOGIN", payload:user})
-          
-          })
-          .catch((error) => {
-            setError(true);
-          });
-      };
-
     return (
         <div className="relative container   m-auto px-6 text-gray-500 md:px-12 xl:px-40">
             <div className="m-auto space-y-8 md:w-8/12 lg:w-full ">
@@ -88,7 +27,7 @@ const SignIn = () => {
                             <h2 className="mb-8 text-2xl text-cyan-900 font-bold">
                                 Sign in to your account
                             </h2>
-                            <form action="" className="space-y-8" onSubmit={formik.handleSubmit}> 
+                            <form action="" className="space-y-8">
                                 <div className="space-y-2">
                                     <label for="email" className="text-gray-700"></label>
                                     <input
@@ -96,17 +35,10 @@ const SignIn = () => {
                                         name="email"
                                         id="email"
                                         placeholder="email"
-                                        onChange={formik.handleChange}
-                                        value={formik.values.email}
                                         className="block w-full px-4 py-3 rounded-md border bg-gray-200 border-gray-300 text-gray-600 transition duration-300
         focus:ring-2 focus:ring-sky-300 focus:outline-none
         invalid:ring-2 "
                                     />
-                                    {formik.errors.email ? (
-                                        <span className="text-red-600 font-roboto text-normal absolute translate-y-4 m-0 flex">
-                                            {formik.errors.email}
-                                        </span>
-                                    ) : null}
                                 </div>
                                 <div>
                                     <div className="flex items-center justify-between">
@@ -123,35 +55,12 @@ const SignIn = () => {
                                         id="password"
                                         placeholder="password"
                                         minLength="8"
-                                        onChange={formik.handleChange}
-                                        value={formik.values.password}
                                         required
                                         className="block w-full px-4  bg-gray-200 py-3 rounded-md border border-gray-300 text-gray-600 transition duration-300
                                             focus:ring-2 focus:ring-sky-300 focus:outline-none
                                             invalid:ring-2 "
                                     />
-                                    <div className=" flex w-full mt-4 flex-row h-11   text-center justify-center">
-                                        <button
-                                            type="button"
-                                            className="text-white 1  justify-center  text-xs w-full bg-[#3f4d5f] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-md  py-1 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
-                                        >
-                                            <svg
-                                                className="mr-2 -ml-1 w-4 h-4"
-                                                aria-hidden="true"
-                                                focusable="false"
-                                                data-prefix="fab"
-                                                data-icon="github"
-                                                role="img"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 496 512"
-                                            >
-                                                <path
-                                                    fill="currentColor"
-                                                    d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
-                                                ></path>
-                                            </svg>
-                                            Sign in with Github
-                                        </button>
+                                    <div className=" flex w-full mt-4 flex-row h-14   text-center justify-center">
                                         <button
                                             type="button"
                                             className="text-white justify-center  text-xs w-full bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-md  py-1 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55  mb-2"
@@ -174,15 +83,15 @@ const SignIn = () => {
                                             Sign in with Google
                                         </button>
                                     </div>
+                                    <button
+                                        type="submit"
+                                        className="w-full mt-1 py-3 px-6 rounded-md bg-sky-600
+                                        focus:bg-sky-700 active:bg-sky-500"
+                                    >
+                                        <span className="text-white">Continue</span>
+                                    </button>
                                 </div>
 
-                                <button
-                                    type="submit"
-                                    className="w-full py-3 px-6 rounded-md bg-sky-600
-                                        focus:bg-sky-700 active:bg-sky-500"
-                                >
-                                    <span className="text-white">Continue</span>
-                                </button>
                                 <p className="border-t pt-6 text-sm">
                                     Don't have an account ?
                                     <Link to="/signup" className="text-sky-500  ml-2">
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,61 +4,7 @@ import { Link } from 'react-router-dom'
 import { useFormik } from 'formik'
 import { useEffect, useState } from 'react'
 
-const validate = (values) => {
-    const errors = {}
-
-    if (!values.firstName) {
-        errors.firstName = 'Required'
-    } else if (values.firstName.length > 15) {
-        errors.firstName = 'Must be 15 characters or less'
-    }
-
-    if (!values.lastName) {
-        errors.lastName = 'Required'
-    } else if (values.lastName.length > 20) {
-        errors.lastName = 'Must be 20 characters or less'
-    }
-
-    if (!values.email) {
-        errors.email = 'Required'
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address'
-    }
-
-    return errors
-}
-
-export const SignUp = () => {
-    const [password, setPassword] = useState('')
-    const [email, setEmail] = useState('')
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-
-    useEffect(() => {
-        setPassword('')
-        setEmail('')
-        setFirstName('')
-        setLastName('')
-    })
-
-    const formik = useFormik({
-        initialValues: {
-            firstName: '',
-            lastName: '',
-            email: '',
-            password: '',
-        },
-
-        validate,
-
-        onSubmit: (values) => {
-            JSON.stringify(values, null, 2)
-            setPassword(values.password)
-            setFirstName(values.firstName)
-            setLastName(values.lastName)
-            setEmail(values.email)
-        },
-    })
+const SignUp = () => {
 
     return (
         <div className="relative container m-auto px-6  text-gray-500 md:px-12 xl:px-40">
@@ -78,7 +24,7 @@ export const SignUp = () => {
                             <h2 className="mb-8 text-2xl text-cyan-900 font-bold">
                                 Create Account
                             </h2>
-                            <form onSubmit={formik.handleSubmit} className="space-y-8">
+                            <form className="space-y-4">
                                 <div className="space-y-2">
                                     <div className="flex gap-x-3">
                                         <label for="firsName" className="hidden"></label>
@@ -87,8 +33,6 @@ export const SignUp = () => {
                                             name="firstName"
                                             id="firsName"
                                             placeholder="First Name"
-                                            onChange={formik.handleChange}
-                                            value={formik.values.firstName}
                                             required
                                             className="block w-fifty px-4 py-2 rounded-md border bg-gray-200 border-gray-300 text-gray-600 transition duration-300
       focus:ring-2 focus:ring-sky-300 focus:outline-none
@@ -100,8 +44,6 @@ export const SignUp = () => {
                                             name="lastName"
                                             id="lastName"
                                             placeholder="Last Name"
-                                            onChange={formik.handleChange}
-                                            value={formik.values.lastName}
                                             required
                                             className="block w-fifty px-4 py-2 rounded-md border bg-gray-200 border-gray-300 text-gray-600 transition duration-300
       focus:ring-2 focus:ring-sky-300 focus:outline-none
@@ -117,17 +59,10 @@ export const SignUp = () => {
                                         id="email"
                                         placeholder="email"
                                         required
-                                        onChange={formik.handleChange}
-                                        value={formik.values.email}
                                         className="block w-full px-4 py-3 rounded-md border bg-gray-200 border-gray-300 text-gray-600 transition duration-300
       focus:ring-2 focus:ring-sky-300 focus:outline-none
       invalid:ring-2 "
                                     />
-                                    {formik.errors.email ? (
-                                        <span className="text-red-600 font-roboto text-normal absolute m-0 flex">
-                                            {formik.errors.email}
-                                        </span>
-                                    ) : null}
                                 </div>
                                 <div>
                                     <div className="flex items-center justify-between">
@@ -143,31 +78,10 @@ export const SignUp = () => {
                                           focus:ring-2 focus:ring-sky-300 focus:outline-none
                                           invalid:ring-2 invalid:ring-red-400"
                                     />
-                                    <div className=" flex w-full mt-4 flex-row h-11 text-center justify-center">
+                                    <div className=" flex w-full mb-4 mt-4 flex-row h-11 text-center justify-center">
                                         <button
                                             type="button"
-                                            className="text-white  justify-center   text-xs w-full bg-[#3f4d5f] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-md   text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
-                                        >
-                                            <svg
-                                                className="mr-2 -ml-1 w-4 h-4"
-                                                aria-hidden="true"
-                                                focusable="false"
-                                                data-prefix="fab"
-                                                data-icon="github"
-                                                role="img"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 496 512"
-                                            >
-                                                <path
-                                                    fill="currentColor"
-                                                    d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
-                                                ></path>
-                                            </svg>
-                                            Sign up with Github
-                                        </button>
-                                        <button
-                                            type="button"
-                                            className="text-white justify-center  text-xs w-full bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-md   text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mb-2"
+                                            className="text-white justify-center  text-xs w-full bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-md   text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 "
                                         >
                                             <svg
                                                 className="mr-2 -ml-1 w-4 h-4"
@@ -187,16 +101,14 @@ export const SignUp = () => {
                                             Sign up with Google
                                         </button>
                                     </div>
-                                </div>
-
-                                <button
-                                    type="submit"
-                                    className="w-full py-3 px-6 rounded-md bg-sky-600
+                                    <button
+                                        type="submit"
+                                        className="w-full py-3 px-6 rounded-md bg-sky-600
                                       focus:bg-sky-700 active:bg-sky-500"
-                                >
-                                    <span className="text-white">Continue</span>
-                                </button>
-
+                                    >
+                                        <span className="text-white">Continue</span>
+                                    </button>
+                                </div>
                                 <p className="border-t pt-6 text-sm">
                                     You already have an account ?
                                     <Link to="/login" className="text-sky-500  ml-2">
