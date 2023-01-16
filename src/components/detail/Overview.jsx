69bf4f82df62fc02d5404326aba5d43c9a7b50cf
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2023-01-16 15:53:40 +0300

    chrome optimization

--- src/components/detail/Overview.jsx
+++ src/components/detail/Overview.jsx
@@ -1,14 +1,12 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import data from "../../../Data";
-import {BsCurrencyDollar} from 'react-icons/bs'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import data from '../../../Data';
+import { BsCurrencyDollar } from 'react-icons/bs';
 
 const Overview = () => {
   const [detail, setDetail] = useState([]);
   let { id } = useParams();
   const baseUrl = `https://api.themoviedb.org/3/movie/${id}?`;
-
 
   useEffect(() => {
     fetch(baseUrl + data.requestSearchMovie)
@@ -19,43 +17,66 @@ const Overview = () => {
       });
   }, [id]);
 
-
   return (
- <section>
-     <div className="w-full px-2  h-full flex flex-col flex-wrap sm:px-5 md:px-10 lg:px-14 mt-8 gap-y-12">
-<div className="translate-x-2  gap-y-12 px-2 flex flex-col  ">
-<div className="">
-    <h3 className="italic font-roboto font-medium text-black dark:text-white text-xl block text-center">{detail.tagline ? <blockquote><q>{detail.tagline}</q></blockquote> : <></>}</h3>
+    <section>
+      <div className="w-full px-2  h-full flex flex-col flex-wrap sm:px-5 md:px-10 lg:px-14 mt-8 gap-y-12">
+        <div className="translate-x-2  gap-y-12 px-2 flex flex-col  ">
+          <div className="">
+            <h3 className="italic font-roboto font-medium text-black dark:text-white text-xl block text-center">
+              {detail.tagline ? (
+                <blockquote>
+                  <q>{detail.tagline}</q>
+                </blockquote>
+              ) : (
+                <></>
+              )}
+            </h3>
+          </div>
+          <div className="flex flex-col gap-y-4">
+            <h3 className="font-roboto text-gray-900 dark:text-gray-100 text-xl text-center md:text-left">
+              Overview
+            </h3>
+            <p className="font-roboto text-gray-800 dark:text-gray-100 text-md text-justify ">
+              {detail.overview}
+            </p>
+          </div>
+          <div className="flex flex-col gap-y-1">
+            <h3 className="font-roboto text-gray-800 dark:text-gray-100 text-lg text-center md:text-left">
+              Details
+            </h3>
+            <p className="font-roboto text-gray-600 dark:text-gray-400 text-sm text-center md:text-left">
+              Original Title: {detail.original_title}
+            </p>
+            <p className="font-roboto text-gray-600 dark:text-gray-400 text-sm text-center md:text-left">
+              Release Date: {detail.release_date}
+            </p>
+          </div>
+        </div>
+        <div className="stats shadow flex flex-wrap md:flex-nowrap bg-card-second rounded-md ">
+          <div className="stat">
+            <div className="stat-title text-white">Budged</div>
+            <div className="stat-value text-primary flex border-b border-gray-500 sm:border-0">
+              {detail.budget}{' '}
+              <span>
+                <BsCurrencyDollar size={23} className="text-gray-300 translate-y-3" />
+              </span>{' '}
+            </div>
+          </div>
+          <div className="stat ">
+            <div className="stat-title text-white ">Vote Count</div>
+            <div className="stat-value text-secondary border-b border-gray-500 sm:border-0 ">
+              {detail.vote_count}
+            </div>
+          </div>
+          <div className="stat ">
+            <div className="stat-title text-white">Popularity</div>
+            <div className="stat-value border-b border-gray-500 sm:border-0 text-white">
+              {detail.popularity}
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="flex flex-col gap-y-4">
-      <h3 className="font-roboto text-gray-900 dark:text-gray-100 text-xl text-center md:text-left">Overview</h3>
-      <p className="font-roboto text-gray-800 dark:text-gray-100 text-md text-justify ">{detail.overview}</p>
-      </div>
-      <div className="flex flex-col gap-y-1">
-      <h3 className="font-roboto text-gray-800 dark:text-gray-100 text-lg text-center md:text-left">Details</h3>
-      <p className="font-roboto text-gray-600 dark:text-gray-400 text-sm text-center md:text-left">Original Title: {detail.original_title}</p>
-      <p className="font-roboto text-gray-600 dark:text-gray-400 text-sm text-center md:text-left">Release Date: {detail.release_date}</p>
-      </div>
-</div>
-  <div className="stats shadow flex flex-wrap md:flex-nowrap bg-card-second rounded-md ">
-  <div className="stat  ">
-    <div className="stat-title ">Budged</div>
-    <div className="stat-value text-primary flex border-b border-gray-500 sm:border-0">{detail.budget} <span><BsCurrencyDollar size={23} className='text-gray-300 translate-y-3'/></span> </div>
-  </div>
-  <div className="stat ">
-    <div className="stat-title ">Vote Count</div>
-    <div className="stat-value text-secondary border-b border-gray-500 sm:border-0 ">{detail.vote_count}</div>
-  </div>
-  <div className="stat ">
-    <div className="stat-title">Popularity</div>
-    <div className="stat-value border-b border-gray-500 sm:border-0">{detail.popularity}</div>
-  </div>
-
-</div>
-      </div>
- </section>
-
-
+    </section>
   );
 };
 
